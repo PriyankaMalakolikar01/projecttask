557586ddc932bf6ce00d07176304da16ef734453
PriyankaMalakolikar01 <priyanka.malakolikar@clairvoyantsoft.com>
2021-07-26 11:43:22 +0530

    changed faviconicon

--- src/ProgressBar/welcome.js
+++ src/ProgressBar/welcome.js
@@ -1,41 +1,34 @@
 import React, { Component } from 'react'
 import './ProgressBar.css'
 export default class Welcome extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-            progress : 0
-        }
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      progress: 0
     }
-    componentDidMount() {
-        let counter = 1;
-        const interval = setInterval(() => {
-            counter++;
-            this.setState({
-                progress : counter
-            })
-            if(counter == 100) {
-                clearInterval(interval);
-            }
-        }, 100);
+  }
+  componentDidMount() {
+    let counter = 1;
+    const interval = setInterval(() => {
+      counter++;
+      this.setState({
+        progress: counter
+      })
+      if (counter == 100) {
+        clearInterval(interval);
       }
-    updateProgress = (field, val) => {
-        this.setState({ [field]: val });
-      };
-    render() {
-        return (
-            <div>
-                Welcome Shital Beta Progress : { this.state.progress }
-               {/* <div className='progress' state={{width:'400' }}> Progress : { this.state.progress }</div> */}
-                {/* <button
-            onClick={() =>
-              this.updateProgress("progress", this.state.progress+ 0.1)
-            }
-          >
-            Add 10%
-          </button> */}
-            </div>
-        )
-    }
+    }, 100);
+  }
+  updateProgress = (field, val) => {
+    this.setState({ [field]: val });
+  };
+  render() {
+    return (
+      <div>
+        Progress : { this.state.progress}
+        
+      </div>
+    )
+  }
 }
